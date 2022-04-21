716d231ad4d40aedd277c6fe712dfe62b5526eb5
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-04-21 08:32:31 +0200

    Support repositories containing many go modules

--- commands.go
+++ commands.go
@@ -3,7 +3,7 @@ package buildgo
 // AddCommands adds go and git commands
 func AddCommands(commands map[string]interface{}) {
 	commands["git/fetch"] = GitFetch
-	commands["dev/goimports"] = GoImports
 	commands["dev/lint"] = GoLint
+	commands["dev/tidy"] = GoModTidy
 	commands["dev/test"] = GoTest
 }
