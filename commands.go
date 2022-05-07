ba7eb1a57d006a27d88aeedefa8fe6f20415695f
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-05-07 18:47:58 +0200

    Upgrade `build`

--- commands.go
+++ commands.go
@@ -1,9 +1,11 @@
 package buildgo
 
+import "github.com/outofforest/build"
+
 // AddCommands adds go and git commands
-func AddCommands(commands map[string]interface{}) {
-	commands["git/fetch"] = GitFetch
-	commands["dev/lint"] = GoLint
-	commands["dev/tidy"] = GoModTidy
-	commands["dev/test"] = GoTest
+func AddCommands(commands map[string]build.Command) {
+	commands["git/fetch"] = build.Command{Fn: GitFetch, Description: "Fetches changes from repository"}
+	commands["dev/lint"] = build.Command{Fn: GoLint, Description: "Lints go code"}
+	commands["dev/tidy"] = build.Command{Fn: GoModTidy, Description: "Runs go mod tidy"}
+	commands["dev/test"] = build.Command{Fn: GoTest, Description: "Runs go unit tests"}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/outofforest/buildgo
 go 1.16
 
 require (
-	github.com/outofforest/build v1.7.12
+	github.com/outofforest/build v1.8.0
 	github.com/outofforest/libexec v0.2.2
 	github.com/outofforest/logger v0.2.0
 	github.com/ridge/must v0.6.0
