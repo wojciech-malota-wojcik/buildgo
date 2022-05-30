10316c8e0fa08e954b3afbb798a8f5a7eeb4c5ac
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-05-30 18:36:04 +0200

    Upgrade tools

--- tools.go
+++ tools.go
@@ -9,9 +9,9 @@ import (
 var tools = map[string]build.Tool{
 	"go": {
 		Name:    "go",
-		Version: "1.18.1",
-		URL:     "https://go.dev/dl/go1.18.1.linux-amd64.tar.gz",
-		Hash:    "sha256:b3b815f47ababac13810fc6021eb73d65478e0b2db4b09d348eefad9581a2334",
+		Version: "1.18.2",
+		URL:     "https://go.dev/dl/go1.18.2.linux-amd64.tar.gz",
+		Hash:    "sha256:e54bec97a1a5d230fc2f9ad0880fcbabb5888f30ed9666eca4a91c5a32e86cbc",
 		Binaries: []string{
 			"go/bin/go",
 			"go/bin/gofmt",
@@ -19,11 +19,11 @@ var tools = map[string]build.Tool{
 	},
 	"golangci": {
 		Name:    "golangci",
-		Version: "1.45.2",
-		URL:     "https://github.com/golangci/golangci-lint/releases/download/v1.45.2/golangci-lint-1.45.2-linux-amd64.tar.gz",
-		Hash:    "sha256:595ad6c6dade4c064351bc309f411703e457f8ffbb7a1806b3d8ee713333427f",
+		Version: "1.46.2",
+		URL:     "https://github.com/golangci/golangci-lint/releases/download/v1.46.2/golangci-lint-1.46.2-linux-amd64.tar.gz",
+		Hash:    "sha256:242cd4f2d6ac0556e315192e8555784d13da5d1874e51304711570769c4f2b9b",
 		Binaries: []string{
-			"golangci-lint-1.45.2-linux-amd64/golangci-lint",
+			"golangci-lint-1.46.2-linux-amd64/golangci-lint",
 		},
 	},
 }
