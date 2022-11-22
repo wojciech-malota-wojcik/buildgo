2ac0025fd67443ee89f0d64e4d232576eb2e4d69
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-11-22 16:49:18 +0100

    Install proto generators

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/outofforest/buildgo
 go 1.16
 
 require (
-	github.com/outofforest/build v1.8.17
+	github.com/outofforest/build v1.8.18
 	github.com/outofforest/libexec v0.3.6
 	github.com/outofforest/logger v0.3.3
 	github.com/pkg/errors v0.9.1
 	github.com/ridge/must v0.6.0
+	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/zap v1.23.0
 )
--- tools.go
+++ tools.go
@@ -29,6 +29,28 @@ var tools = map[string]build.Tool{
 			"golangci-lint-1.50.1-linux-amd64/golangci-lint",
 		},
 	},
+
+	// https://github.com/protocolbuffers/protobuf/releases/
+	"protoc": {
+		Name:    "protoc",
+		Version: "21.9",
+		URL:     "https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-linux-x86_64.zip",
+		Hash:    "sha256:3cd951aff8ce713b94cde55e12378f505f2b89d47bf080508cf77e3934f680b6",
+		Binaries: []string{
+			"bin/protoc",
+		},
+	},
+
+	// https://github.com/protocolbuffers/protobuf-go/releases
+	"protoc-gen-go": {
+		Name:    "protoc-gen-go",
+		Version: "1.28.1",
+		URL:     "https://github.com/protocolbuffers/protobuf-go/releases/download/v1.28.1/protoc-gen-go.v1.28.1.linux.amd64.tar.gz",
+		Hash:    "sha256:5c5802081fb9998c26cdfe607017a677c3ceaa19aae7895dbb1eef9518ebcb7f",
+		Binaries: []string{
+			"protoc-gen-go",
+		},
+	},
 }
 
 // InstallAll installs all go tools
@@ -39,6 +61,18 @@ func InstallAll(ctx context.Context) error {
 // EnsureGo ensures that go is installed
 func EnsureGo(ctx context.Context) error {
 	return build.EnsureTool(ctx, tools["go"])
+}
+
+// EnsureProtoC ensures that protoc is installed
+func EnsureProtoC(ctx context.Context) error {
+	return build.EnsureTool(ctx, tools["protoc"])
+}
+
+// EnsureGoProto ensures that go proto generator is installed
+func EnsureGoProto(ctx context.Context, deps build.DepsFunc) error {
+	deps(EnsureProtoC)
+
+	return build.EnsureTool(ctx, tools["protoc-gen-go"])
 }
 
 // EnsureGolangCI ensures that golangci is installed
