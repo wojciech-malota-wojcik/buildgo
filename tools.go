c1f53a32c80df01e3130c0b3f662a0ddbe2c1e95
Wojciech Malota-Wojcik <wojtek@exw.co>
2023-05-28 16:41:17 +0200

    Bump `build` and remove not used tools

--- tools.go
+++ tools.go
@@ -13,9 +13,9 @@ var tools = map[string]build.Tool{
 		Version: "1.20.1",
 		URL:     "https://go.dev/dl/go1.20.1.linux-amd64.tar.gz",
 		Hash:    "sha256:000a5b1fca4f75895f78befeb2eecf10bfff3c428597f3f1e69133b63b911b02",
-		Binaries: []string{
-			"go/bin/go",
-			"go/bin/gofmt",
+		Binaries: map[string]string{
+			"tools/go":    "go/bin/go",
+			"tools/gofmt": "go/bin/gofmt",
 		},
 	},
 
@@ -25,30 +25,8 @@ var tools = map[string]build.Tool{
 		Version: "1.51.2",
 		URL:     "https://github.com/golangci/golangci-lint/releases/download/v1.51.2/golangci-lint-1.51.2-linux-amd64.tar.gz",
 		Hash:    "sha256:4de479eb9d9bc29da51aec1834e7c255b333723d38dbd56781c68e5dddc6a90b",
-		Binaries: []string{
-			"golangci-lint-1.51.2-linux-amd64/golangci-lint",
-		},
-	},
-
-	// https://github.com/protocolbuffers/protobuf/releases/
-	"protoc": {
-		Name:    "protoc",
-		Version: "21.9",
-		URL:     "https://github.com/protocolbuffers/protobuf/releases/download/v21.9/protoc-21.9-linux-x86_64.zip",
-		Hash:    "sha256:3cd951aff8ce713b94cde55e12378f505f2b89d47bf080508cf77e3934f680b6",
-		Binaries: []string{
-			"bin/protoc",
-		},
-	},
-
-	// https://github.com/protocolbuffers/protobuf-go/releases
-	"protoc-gen-go": {
-		Name:    "protoc-gen-go",
-		Version: "1.28.1",
-		URL:     "https://github.com/protocolbuffers/protobuf-go/releases/download/v1.28.1/protoc-gen-go.v1.28.1.linux.amd64.tar.gz",
-		Hash:    "sha256:5c5802081fb9998c26cdfe607017a677c3ceaa19aae7895dbb1eef9518ebcb7f",
-		Binaries: []string{
-			"protoc-gen-go",
+		Binaries: map[string]string{
+			"tools/golangci-lint": "golangci-lint-1.51.2-linux-amd64/golangci-lint",
 		},
 	},
 }
