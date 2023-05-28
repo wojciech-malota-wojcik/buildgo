d25fa58ca13da82867bd3bb74d8ad3e75e554758
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-05-28 21:45:40 +0200

    Add command rebuilding the build tool (#56)

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import "github.com/outofforest/build"
 
 // AddCommands adds go and git commands
 func AddCommands(commands map[string]build.Command) {
+	commands["build/me"] = build.Command{Fn: rebuildMe, Description: "Rebuilds the building tool"}
 	commands["git/fetch"] = build.Command{Fn: GitFetch, Description: "Fetches changes from repository"}
 	commands["dev/lint"] = build.Command{Fn: GoLint, Description: "Lints go code"}
 	commands["dev/tidy"] = build.Command{Fn: GoModTidy, Description: "Runs go mod tidy"}
