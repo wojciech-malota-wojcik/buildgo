36d9f20eb18436e5f7ea3a71a936d421c80ac295
Wojciech Malota-Wojcik <wojtek@exw.co>
2023-06-21 15:58:52 +0200

    Install go globally

--- tools.go
+++ tools.go
@@ -9,24 +9,26 @@ import (
 var tools = map[string]build.Tool{
 	// https://go.dev/dl/
 	"go": {
-		Name:    "go",
-		Version: "1.20.4",
-		URL:     "https://go.dev/dl/go1.20.4.linux-amd64.tar.gz",
-		Hash:    "sha256:698ef3243972a51ddb4028e4a1ac63dc6d60821bf18e59a807e051fee0a385bd",
+		Name:     "go",
+		Version:  "1.20.4",
+		IsGlobal: true,
+		URL:      "https://go.dev/dl/go1.20.4.linux-amd64.tar.gz",
+		Hash:     "sha256:698ef3243972a51ddb4028e4a1ac63dc6d60821bf18e59a807e051fee0a385bd",
 		Binaries: map[string]string{
-			"tools/go":    "go/bin/go",
-			"tools/gofmt": "go/bin/gofmt",
+			"go":    "go/bin/go",
+			"gofmt": "go/bin/gofmt",
 		},
 	},
 
 	// https://github.com/golangci/golangci-lint/releases/
 	"golangci": {
-		Name:    "golangci",
-		Version: "1.52.2",
-		URL:     "https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.tar.gz",
-		Hash:    "sha256:c9cf72d12058a131746edd409ed94ccd578fbd178899d1ed41ceae3ce5f54501",
+		Name:     "golangci",
+		Version:  "1.52.2",
+		IsGlobal: true,
+		URL:      "https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.tar.gz",
+		Hash:     "sha256:c9cf72d12058a131746edd409ed94ccd578fbd178899d1ed41ceae3ce5f54501",
 		Binaries: map[string]string{
-			"tools/golangci-lint": "golangci-lint-1.52.2-linux-amd64/golangci-lint",
+			"golangci-lint": "golangci-lint-1.52.2-linux-amd64/golangci-lint",
 		},
 	},
 }
