5fb82d6cdac63bc9ad05377404ec88decaf4fb75
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-08-27 07:57:51 +0200

    Common entrypoint for builders

--- build/cmd/builder/main.go
+++ build/cmd/builder/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/outofforest/build/v2"
 	"github.com/outofforest/build/v2/pkg/tools/git"
-	"github.com/outofforest/tools/pkg/tools"
+	tmain "github.com/outofforest/tools"
 )
 
 func main() {
@@ -12,5 +12,5 @@ func main() {
 		git.Commands,
 		commands,
 	)
-	build.Main("outofforest", tools.Version())
+	tmain.Main()
 }
