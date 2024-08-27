1f0f61c34bc5bce302d30f7af6e6323364dcea86
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-08-27 21:42:03 +0200

    Foundry fix (#77)
    * Fix foundry
    
    * Fix foundry

--- pkg/tools/evm/tools.go
+++ pkg/tools/evm/tools.go
@@ -16,7 +16,8 @@ var t = []tools.Tool{
 		Version: "nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce",
 		Sources: tools.Sources{
 			tools.PlatformLinuxAMD64: {
-				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz",
+				//nolint:lll
+				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_linux_amd64.tar.gz",
 				Hash: "sha256:2c75c62fab2a521938fd2a5eec6e97f9718eb0b6802852f54f1d886100fe8eb0",
 				Links: map[string]string{
 					"bin/anvil": "anvil",
@@ -25,7 +26,8 @@ var t = []tools.Tool{
 				},
 			},
 			tools.PlatformDarwinAMD64: {
-				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz",
+				//nolint:lll
+				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_darwin_amd64.tar.gz",
 				Hash: "sha256:cf853e416cf9358174bf4fcf603b5c263aed456842b9c78661c4d77654133b7a",
 				Links: map[string]string{
 					"bin/anvil": "anvil",
@@ -34,7 +36,8 @@ var t = []tools.Tool{
 				},
 			},
 			tools.PlatformDarwinARM64: {
-				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz",
+				//nolint:lll
+				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_darwin_arm64.tar.gz",
 				Hash: "sha256:af157f6daac33bb4b955875e777c52d7d022e6471ed2bf1cddba9869ed5707f0",
 				Links: map[string]string{
 					"bin/anvil": "anvil",
